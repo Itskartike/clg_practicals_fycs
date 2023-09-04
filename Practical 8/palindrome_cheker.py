63c223df4874540c8db4603e9f7e299e3b1b5a91
Hukum Kumar <111696853+Itskartike@users.noreply.github.com>
2023-09-04 09:19:09 +0530

    Add files via upload

---
+++ Practical 8/palindrome_cheker.py
@@ -0,0 +1,8 @@
+# to check whether the string is palindrome
+s1=input("Enter a string")
+s2=s1[::-1]
+print(s2)
+if s1==s2:
+    print(s1,"is palindrome!")
+else:
+    print(s1,"is not a palindrome!s")
